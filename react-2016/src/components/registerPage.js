8cfa25c9d3be1c5b90a4d43226039b2e338c44be
PaulDiaconescu <diaconescu.paul11@gmail.com>
2016-07-26 13:29:04 +0300

    Add files via upload

---
+++ react-2016/src/components/registerPage.js
@@ -0,0 +1,35 @@
+"use strict";
+
+var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
+var RegisterForm = require('./RegisterForm');
+
+
+var Register = React.createClass({
+	render: function() {
+		//noinspection JSDuplicatedDeclaration
+        var styles = {
+        backgroundImage: "none",
+		fontSize: 15,
+        width: "290px",
+        height: "330px",
+        color: "#ffffff"
+        };
+
+		return (
+
+            <center>
+            <div className="Register Form" style={styles}>
+
+				<RegisterForm />
+			</div>
+             </center>
+		);
+	}
+});
+
+
+
+module.exports = Register;
+
